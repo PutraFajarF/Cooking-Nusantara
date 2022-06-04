7d7d1f27f9f33e81e6d709113efaa8fb0b55a07c
Putra Fajar <putrautalls@gmail.com>
2022-06-05 02:30:41 +0700

    create 4 pages components, install react-route-dom to the project & link them for navigation using React-Router-DOM 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// page component
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+
+// styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<Home />}/>
+          <Route path='create' element={<Create />} />
+          <Route path='search' element={<Search />} />
+          <Route path='recipe/:id' element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
