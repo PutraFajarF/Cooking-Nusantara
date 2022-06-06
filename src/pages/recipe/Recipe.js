4bf0a9e8cc30caa62985edbdf658f46be352f759
Putra Fajar <putrautalls@gmail.com>
2022-06-06 13:29:51 +0700

    added useParams function in Recipe.js to fetching Single Recipe

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,8 +1,19 @@
+import { useFetch } from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+
+// styles
 import './Recipe.css';
 
 const Recipe = () => {
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/' + id;
+  const { data: recipe, isLoading, error } = useFetch(url);
   return(
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading..</p>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
   )
 };
 
