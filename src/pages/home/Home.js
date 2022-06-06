1c5e34d6671c5fda05238f8a2af2146d4c501fee
Putra Fajar <putrautalls@gmail.com>
2022-06-06 12:46:45 +0700

    added RecipeList Component to the project

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,10 @@
 import { useFetch } from '../../hooks/useFetch';
+
+//styles
 import './Home.css';
+
+// components
+import RecipeList from '../../components/RecipeList';
 
 const Home = () => {
   const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
@@ -7,9 +12,7 @@ const Home = () => {
     <div className='home'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
-      {data && data.map((recipe) => (
-        <h2 key={recipe.id}>{recipe.title}</h2>
-      ))}
+      {data && <RecipeList recipes={data}/>}
     </div>
   )
 };
