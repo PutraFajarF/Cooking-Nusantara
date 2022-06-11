2d04e55dfaf0235f6e90ff2ac6f795c64f4b7d83
Putra Fajar <putrautalls@gmail.com>
2022-06-12 03:13:41 +0700

    apply onSnapShot() method in Home.js for real time collection data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,26 +13,29 @@ const Home = () => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    setIsPending(true)
+    setIsPending(true);
 
-    projectFirestore.collection('recipes').get().then(snapshot => {
+    const unsub = projectFirestore.collection('recipes').onSnapshot((snapshot) => {
       if (snapshot.empty) {
-        setError('No recipes to load')
-        setIsPending(false)
+        setError('No recipes to load');
+        setIsPending(false);
       } else {
-        let results = []
+        let results = [];
         snapshot.docs.forEach(doc => {
           // console.log(doc)
           results.push({ ...doc.data(), id: doc.id })
-        })
-        setData(results)
-        setIsPending(false)
+        });
+        setData(results);
+        setIsPending(false);
       }
-    }).catch(err => {
-      setError(err.message)
-      setIsPending(false)
+    }, (err) => {
+      setError(err.message);
+      setIsPending(false);
     })
-  }, [])
+
+    return () => unsub();
+  }, []);
+
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
