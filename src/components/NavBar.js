9b252833ca3d033de52ed039b4dfb631ab9a20e6
Putra Fajar <putrautalls@gmail.com>
2022-06-11 23:18:49 +0700

    Test to access Context Values in NavBar.js Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 // styles
 import './NavBar.css';
@@ -6,9 +8,11 @@ import './NavBar.css';
 // components
 import Searchbar from "./Searchbar";
 
+
 const NavBar = () => {
+  const { color } = useContext(ThemeContext)
   return(
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Nusantara</h1>
